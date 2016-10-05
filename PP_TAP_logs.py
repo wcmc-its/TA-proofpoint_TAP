02b256612d129f4460b4620ca4676c9660ec405d
Bryan Fisher <brf2010@med.cornell.edu>
2016-10-05 15:04:37 -0400

    fixed up time zone stuff so everything uses UTC

--- PP_TAP_logs.py
+++ PP_TAP_logs.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+
 import requests
-import datetime
+from datetime import datetime, timezone, timedelta
 import json
 from pprint import pprint
+from urllib.parse import quote_plus
 
 # credentials for the proofpoint API
 username = "redacted"
@@ -13,16 +16,18 @@ time_range_minutes = 1
 # base proofpoint TAP API url
 base_url = "https://tap-api.proofpoint.com/v1/siem/all?format=JSON?interval="
 
-#now = datetime.datetime.now() # get current time
-now = datetime.datetime(year=2016, month=10, day=4, hour=12, minute=12) # temporary static time for testing
-now = now - datetime.timedelta(microseconds=now.microsecond) # chop off the microseconds because stupid
-minute = datetime.timedelta(minutes=time_range_minutes) # create a time delta object to represent one minute
-then = now - minute # get the time 1 minute ago
-ISO_interval = then.isoformat() + "-0400" + "/" + now.isoformat() + "-0400" # format an ISO8601-compliant interval string for the API
+now = datetime.now(timezone.utc) # get current time
+#now = datetime(year=2016, month=10, day=4, hour=12, minute=12, tzinfo=timezone.utc) # temporary static time for testing
+now = now - timedelta(microseconds=now.microsecond) # chop off the microseconds because stupid
+time_range = timedelta(minutes=time_range_minutes) # create a time delta object to represent our time range
+then = now - time_range # get the time 1 minute ago
+ISO_interval = then.isoformat() + "/" + now.isoformat() # format an ISO8601-compliant interval string for the API
+ISO_interval = quote_plus(ISO_interval) # encode the ISO interval for the http request
 
 # query the API
 page = requests.get('https://tap-api-v2.proofpoint.com/v2/siem/all?format=JSON&interval=%s' %(ISO_interval), auth=(username, password))
-
+#print(page.url)
+#print(page.text)
 # convert API output to JSON object
 api_output = json.loads(page.text)
 
@@ -31,21 +36,21 @@ api_output = json.loads(page.text)
 # print each of our objects output
 # malicious URLs that were blocked
 for click in api_output["clicksBlocked"]:
-    print(click["clickTime"] + " clickBlocked " + str(click))
+    print(api_output['queryEndTime'] + " clickBlocked " + str(click))
 
 # malicious URLs that were clicked on but not blocked
 for click in api_output["clicksPermitted"]:
-    print(click["clickTime"] + " clickPermitted " + str(click))
+    print(api_output['queryEndTime'] + " clickPermitted " + str(click))
 
 # malicious messages that were blocked
 for message in api_output["messagesBlocked"]:
-    print(message["messageTime"] + " messageBlocked " + str(message))
+    print(api_output['queryEndTime'] + " messageBlocked " + str(message))
 
 # malicious messages that were delivered
 for message in api_output["messagesDelivered"]:
-    print(message["messageTime"] + " messageDelivered " + str(message))
+    print(api_output['queryEndTime'] + " messageDelivered " + str(message))
 
 
 # print the shit
 #print(page.url)
-pprint(api_output)
+#pprint(api_output)
